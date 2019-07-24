b58a920c0f54612bec1e4d3f058b3e39cb8aba48
Ali Raheem <github@hadoken.cc>
2019-07-24 19:35:43 +0100

    Add metals

--- raytrace.v
+++ raytrace.v
@@ -4,38 +4,51 @@ import math
 import vec3
 import rand
 
-interface Materialer {
-  scatter(ray vec3.Ray, rec HitRecord) Reflection
-}
+const (
+       BounceDepth = 50
+       ImageWidth = 800
+       ImageHeight = 400
+       Rays = 1000
+)
 
-struct Lambertian {
-   albedo vec3.Vec
-}
-       
-fn (l Lambertian) scatter(ray vec3.Ray, rec HitRecord) Reflection {
+
+fn lambertian_scatter(ray vec3.Ray, rec HitRecord) Reflection {
     target := rec.p + rec.normal + random_point_in_sphere()
     scattered := vec3.Ray{rec.p, target}
-    attenuation := l.albedo
+    attenuation := rec.albedo
     return Reflection{attenuation, scattered}
+}
+
+fn metal_scatter(ray vec3.Ray, rec HitRecord) Reflection {
+    reflected := reflect((ray.b).make_unit(), rec.normal)
+    scattered := vec3.Ray{rec.p, reflected}
+    attenuation := rec.albedo
+    return Reflection{attenuation, scattered}
+}
+
+fn reflect(v vec3.Vec, n vec3.Vec) vec3.Vec {
+        return v - n.mul_scalar(2.0 * v.dot(n))
 }
 
   
 struct HitRecord {
   mut:
-      t f32
+      t f64
       p vec3.Vec
       normal vec3.Vec
-      mat Materialer
+      scatter fn(ray vec3.Ray, rec HitRecord) Reflection
+      albedo vec3.Vec
 }
 
 struct Sphere {
   centre vec3.Vec
-  radius f32
-  mat Materialer
+  radius f64
+  scatter fn(ray vec3.Ray, rec HitRecord) Reflection
+  albedo vec3.Vec
 }
 
 
-fn (s Sphere) hit(r vec3.Ray, t_min f32, t_max f32) ?HitRecord {
+fn (s Sphere) hit(r vec3.Ray, t_min f64, t_max f64) ?HitRecord {
     oc := r.a - s.centre
     a := r.b.dot(r.b)
     b := 2.0 * oc.dot(r.b)
@@ -47,18 +60,11 @@ fn (s Sphere) hit(r vec3.Ray, t_min f32, t_max f32) ?HitRecord {
        (-b + math.sqrt(discriminant))/(2.0 * a)
     }
     if (temp > t_min && temp < t_max) {
-      return HitRecord{temp, r.at(temp), (r.at(temp) - s.centre).div_scalar(s.radius), s.mat}
+      return HitRecord{temp, r.at(temp), (r.at(temp) - s.centre).div_scalar(s.radius), s.scatter, s.albedo}
     }
 
     return error('No hit')
 }
-
-/*
-interface Hitter {
-  hit(r vec3.Ray, t_min f32, t_max f32) ?HitRecord
-}
-*/
-
 
 struct HitList {
   mut:
@@ -66,7 +72,7 @@ struct HitList {
     list_size int
 }
 
-fn (l HitList) hit(r vec3.Ray, t_min f32, t_max f32) ?HitRecord {
+fn (l HitList) hit(r vec3.Ray, t_min f64, t_max f64) ?HitRecord {
   mut closest := t_max
   mut hit_some := false
   mut hit_rec := HitRecord{}
@@ -92,11 +98,12 @@ fn colour(r vec3.Ray, h HitList, depth int) vec3.Vec {
       return a.mul_scalar(1.0 - ic) + b.mul_scalar(ic)
    }
 
-   if (depth < 50) {
-       ref := (rec.mat).scatter(r, rec)
+   if (depth < BounceDepth) {
+       scatterfn := rec.scatter
+       ref := scatterfn(r, rec)
        atten := ref.attenuation
        scat := colour(ref.scatter, h, depth + 1)
-                    // TODO why can't I overload here?
+// TODO why can't I overload here?
        return mul(atten, scat)
    } else {
        return vec3.Vec{0, 0, 0}
@@ -107,17 +114,10 @@ fn mul( a vec3.Vec, b vec3.Vec) vec3.Vec {
       return vec3.Vec{a.x() * b.x(),  a.y() * b.y(), a.z() * b.z()}
 } 
 
-
 struct Reflection {
     attenuation vec3.Vec
     scatter vec3.Ray
 }
-  
-fn reflect(v vec3.Vec, n vec3.Vec) vec3.Vec {
-        return v - n.mul_scalar(2.0 * v.dot(n))
-}
-
-
 
 struct Camera {
     origin vec3.Vec
@@ -126,29 +126,27 @@ struct Camera {
     vertical vec3.Vec
 }
 
-fn (c Camera) get_ray(u f32, v f32) vec3.Ray {
+fn (c Camera) get_ray(u f64, v f64) vec3.Ray {
    return vec3.Ray{c.origin, c.lower_left_corner + c.horizontal.mul_scalar(u) + c.vertical.mul_scalar(v) - c.origin}
 }
 
-fn randf32() f32 {
-   return f32(rand.next(255))/256.0
+fn randf64() f64 {
+   return f64(rand.next(255))/256.0
 }
 
 fn random_point_in_sphere() vec3.Vec {
    for {
-     p := (vec3.Vec{randf32(), randf32(), randf32()} - vec3.Vec{1, 1, 1}).mul_scalar(2)
+     p := (vec3.Vec{randf64(), randf64(), randf64()} - vec3.Vec{1, 1, 1}).mul_scalar(2)
      if p.hypotenuse() < 1 {
         return vec3.Vec{p.x(), p.y(), p.z()}
      }
    }
 }
-
-
        
 fn main() {
-    nx := 200
-    ny := 100
-    ns := 100
+    nx := ImageWidth
+    ny := ImageHeight
+    ns := Rays
     println('P3')
     println('$nx $ny')
     println('255')
@@ -157,18 +155,20 @@ fn main() {
     vert := vec3.Vec{0, 2, 0}
     origin := vec3.Vec{0, 0, 0}
     cam := Camera{origin, llc, hor, vert}
-    lam := Lambertian{vec3.Vec{0.1, 0.1, 0.1}}
-    mut h := HitList{[Sphere{vec3.Vec{0,0,0}, 0, lam}; 4], 4}
-    h.list[0] = Sphere{vec3.Vec{0, -100.5, -1}, 100, lam}
-    h.list[1] = Sphere{vec3.Vec{0, 0, -1}, 0.5, lam}
-    h.list[2] = Sphere{vec3.Vec{0.1, 0, -0.5}, 0.1, lam}
-    h.list[3] = Sphere{vec3.Vec{-0.1, 0, -0.5}, 0.1, lam}
+//    lam := Lambertian{vec3.Vec{0.1, 0.1, 0.1}}
+//    metal := Metal{vec3.Vec{0.8, 0.8, 0.8}}
+    mut h := HitList{[Sphere{vec3.Vec{0,0,0}, 0, lambertian_scatter, vec3.Vec{0,0,0}}; 4], 4}
+    h.list[0] = Sphere{vec3.Vec{0, -100.5, -1}, 100, lambertian_scatter, vec3.Vec{0.8, 0.8, 0.0}}
+    h.list[1] = Sphere{vec3.Vec{0, 0, -1}, 0.5, lambertian_scatter, vec3.Vec{0.8, 0.3, 0.3}}
+    h.list[2] = Sphere{vec3.Vec{1, 0, -1}, 0.5, metal_scatter, vec3.Vec{0.8, 0.6, 0.2}}
+    h.list[3] = Sphere{vec3.Vec{-1, 0, -1}, 0.5, metal_scatter, vec3.Vec{0.8, 0.8, 0.8}}
+    
     for j := ny - 1; j >= 0; j -- {
         for i := 0; i < nx; i++ {
             mut c := vec3.Vec{0, 0, 0}
             for s := 0; s < ns; s++ {
-                u := (f32(i) + randf32())/f32(nx)
-                v := (f32(j) + randf32())/f32(ny)
+                u := (f64(i) + randf64())/f64(nx)
+                v := (f64(j) + randf64())/f64(ny)
                 r := cam.get_ray(u, v)
                 c = c + colour(r, h, 0)
             }
--- vec3.v
+++ vec3.v
@@ -4,27 +4,27 @@ import math
 
 struct Vec {
    mut:
-       i f32
-       j f32
-       k f32
+       i f64
+       j f64
+       k f64
 }
 
 // hypotenuse Calculate the displacement due to Vec
-pub fn (v Vec) hypotenuse() f32 {
+pub fn (v Vec) hypotenuse() f64 {
     return v.i * v.i + v.j * v.j + v.k * v.k
 }
 
-pub fn (v Vec) x() f32 {
+pub fn (v Vec) x() f64 {
     return v.i
 }
-pub fn (v Vec) y() f32 {
+pub fn (v Vec) y() f64 {
     return v.j
 }
-pub fn (v Vec) z() f32 {
+pub fn (v Vec) z() f64 {
     return v.k
 }       
 // length Calculate the abs/mangatude of Vec3 as a Vec
-pub fn (v Vec) length() f32 {
+pub fn (v Vec) length() f64 {
   return math.sqrt(v.hypotenuse())
 }
 
@@ -57,7 +57,7 @@ pub fn (a Vec) * (b Vec) Vec {
 }
 
 // mul_scalar Multiple a vec3 Vec by a scalar
-pub fn (a Vec) mul_scalar(t f32) Vec {
+pub fn (a Vec) mul_scalar(t f64) Vec {
   return Vec {
     a.i * t,
     a.j * t,
@@ -66,7 +66,7 @@ pub fn (a Vec) mul_scalar(t f32) Vec {
 }
 
 // div_scalar Divide Vec by a scalar
-pub fn (a Vec) div_scalar(t f32) Vec {
+pub fn (a Vec) div_scalar(t f64) Vec {
   return Vec {
     a.i / t,
     a.j / t,
@@ -74,7 +74,7 @@ pub fn (a Vec) div_scalar(t f32) Vec {
   }
 }
 // dot Calculate dot product between two vec3 Vecs
-pub fn (v Vec) dot(u Vec) f32 {
+pub fn (v Vec) dot(u Vec) f64 {
   return v.i * u.i + v.j * u.j + v.k * u.k
 }
 
@@ -111,11 +111,11 @@ pub fn (c RGB) str() string {
     return '$c.r $c.g $c.b'
 }
 
-pub fn (c RGB) mul_scalar(k f32) RGB {
+pub fn (c RGB) mul_scalar(k f64) RGB {
     return RGB{
-               u8(f32(c.r) * k),
-               u8(f32(c.g) * k),
-               u8(f32(c.b) * k)
+               u8(f64(c.r) * k),
+               u8(f64(c.g) * k),
+               u8(f64(c.b) * k)
                }
 }
        
@@ -126,7 +126,7 @@ struct Ray {
 }
 
 // at Calculate position along Ray at t.
-pub fn (r Ray) at(t f32) Vec {
+pub fn (r Ray) at(t f64) Vec {
   return r.a + r.b.mul_scalar(t)
 }
 
